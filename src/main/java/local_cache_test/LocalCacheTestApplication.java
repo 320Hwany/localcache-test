5124ddfbdfe9e805b9c25108b868a3d572105dad
Jung-YouHwan <yhwjd99@gmail.com>
2024-02-29 11:46:27 +0900

    feat - 초기 설정

---
+++ src/main/java/local_cache_test/LocalCacheTestApplication.java
@@ -0,0 +1,13 @@
+package local_cache_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocalCacheTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LocalCacheTestApplication.class, args);
+    }
+
+}
